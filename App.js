fba4c82de2da1d670b16afb7051cd0108c4f063e
yavuz <yavuz.akpinar.68@gmail.com>
2021-11-18 21:21:14 +0300

    First commit Tarifka has been created

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+
+export default App;
