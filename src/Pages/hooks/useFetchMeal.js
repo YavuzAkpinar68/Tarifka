e5d101296cea8331348c74d3e87fd17a5d4341fb
yavuz <yavuz.akpinar.68@gmail.com>
2021-11-19 22:25:18 +0300

    A custom hook component has created for meal page

---
+++ src/Pages/hooks/useFetchMeal.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Config from "react-native-config";
+
+const UseFetch = () => {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const fetchData = async () =>{
+    try {
+      const response = await axios.get(Config.API_URL_1)
+      setData(response.data)
+      setLoading(false)
+      } catch (error) {
+        setError("error")
+        setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+  return{data, error, loading}
+}
+export default UseFetch
